d1f319e7af49bcb129ca092b77fc3de473b94ca7
Fahmi <fahmiblackant@gmail.com>
2026-05-15 18:21:19 +0700

    Add Android Capacitor APK support

---
+++ android/app/src/main/java/com/satria/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.satria.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'SATRIA Monitoring',
+        short_name: 'SATRIA',
+        description: 'Sistem Monitoring dan Tracking Aktivitas',
+
+        theme_color: '#0d6efd',
+        background_color: '#ffffff',
+
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
